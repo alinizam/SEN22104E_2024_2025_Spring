91aa28af7d98bd3288bc2b8b585a16d4163724eb
alinizam <alinizam99@gmail.com>
2025-03-12 16:08:48 +0300

    Week 03 lab

---
+++ SEN22104E_2024_2025_Spring/src/main/java/Week03Lab/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week03Lab;
+
+/**
+ *
+ * @author ali.nizam
+ */
+public class Employee {
+    String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+    
+}
