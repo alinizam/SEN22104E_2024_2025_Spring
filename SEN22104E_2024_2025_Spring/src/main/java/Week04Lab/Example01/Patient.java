1f3d1794fb39d47ffd334823e6aa0bc432b94fd0
alinizam <alinizam99@gmail.com>
2025-03-25 09:54:24 +0300

    Week 4 Theory and Lab

---
+++ SEN22104E_2024_2025_Spring/src/main/java/Week04Lab/Example01/Patient.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week04Lab.Example01;
+
+/**
+ *
+ * @author ali.nizam
+ */
+public class Patient {
+    String firstName,lastName,Id,DoctorName;
+
+    public Patient(String firstName, String lastName, String Id, String DoctorName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Id = Id;
+        this.DoctorName = DoctorName;
+    }
+    
+}
